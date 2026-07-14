087bd7c1487c9adac3bb126e460434da62956730
Ulfric Irons <ironsulfric@gmail.com>
2026-07-14 20:51:46 +0100

    Final touches on Selenium framework (barring more tests to run)

---
+++ Pages/HomePage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenQA.Selenium;
+
+public class HomePage : BasePage
+{
+    private const string BaseUrl = "https://www.zigwheels.com";
+
+    public HomePage(IWebDriver driver) : base(driver) { }
+
+    public void NavigateToHomePage()
+    {
+        NavigateTo($"{BaseUrl}/");
+    }
+}
